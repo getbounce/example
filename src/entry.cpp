b244d2190070af67d6c462f160133586ee2b4c73
Arthur Chaloin <arthur.chaloin@gmail.com>
2022-01-29 14:27:52 +0100

    feat: add build type symbol

--- src/entry.cpp
+++ src/entry.cpp
@@ -11,10 +11,16 @@ using Controller = SB::VST::Controller<Schema::ParametersConfig, Schema::State, 
 
 BEGIN_FACTORY_DEF(VENDOR, WEBSITE, EMAIL)
 
-DEF_CLASS2(INLINE_UID_FROM_FUID(ProcessorUID), PClassInfo::kManyInstances, kVstAudioEffectClass, NAME,
+DEF_CLASS2(INLINE_UID_FROM_FUID(::ProcessorUID), PClassInfo::kManyInstances, kVstAudioEffectClass, NAME,
            Vst::kDistributable, CATEGORY, VERSION, kVstVersionString, Processor::createInstance)
 
-DEF_CLASS2(INLINE_UID_FROM_FUID(ControllerUID), PClassInfo::kManyInstances, kVstComponentControllerClass,
+DEF_CLASS2(INLINE_UID_FROM_FUID(::ControllerUID), PClassInfo::kManyInstances, kVstComponentControllerClass,
            NAME "Controller", 0, "", VERSION, kVstVersionString, Controller::createInstance)
 
 END_FACTORY
+
+#ifdef RELEASE
+const bool Development = false;
+#else
+const bool Development = true;
+#endif
